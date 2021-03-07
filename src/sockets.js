84dac52e35caa8942b6ee70c0c17e36958427f0a
Sergey <sergey.abramov996@gmail.com>
2021-03-07 15:24:23 +0300

    add logging

--- src/sockets.js
+++ src/sockets.js
@@ -7,8 +7,9 @@ const webSocketServer = new WebSocket.Server({ port: socketPort });
 logger.info(`Server listening sockets on port ${socketPort}`);
 
 webSocketServer.on('connection', (ws) => {
-  ws.send('Hi there, I am a WebSocket server.I will send you new data cryptocompare service every 5 seconds');
+  logger.info('New socket connection');
 
+  ws.send('Hi there, I am a WebSocket server.I will send you new data cryptocompare service every 5 seconds');
   ws.on('error', (e) => ws.send(e));
 });
 
